b096bad5c12504e254b176680eb9e5ec2ca326e9
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2020-04-20 16:19:30 +0900

    fix: Fix handling of permissions for location data
    * Fixes #18

--- cicerone/app/src/main/java/com/teampower/cicerone/ScrollingActivity.kt
+++ cicerone/app/src/main/java/com/teampower/cicerone/ScrollingActivity.kt
@@ -1,20 +1,23 @@
 package com.teampower.cicerone
 
+import android.Manifest
 import android.content.IntentSender
-import android.location.Location
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_scrolling.*
-import java.util.jar.Manifest
+
+const val MY_PERMISSIONS_REQUEST_LOCATION_ID = 99
 
 
 class ScrollingActivity : AppCompatActivity() {
@@ -29,10 +32,12 @@ class ScrollingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_scrolling)
         setSupportActionBar(toolbar)
 
+        user_location.text = getString(R.string.user_position, "-", "-")
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Fun stuff here", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show()}
+                .setAction("Action", null).show()
+        }
 
         // Setup location services
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -42,17 +47,148 @@ class ScrollingActivity : AppCompatActivity() {
                 locationResult ?: return
                 for (location in locationResult.locations) {
                     Log.v(TAG, location.toString())
+                    user_location.text = getString(
+                        R.string.user_position,
+                        location.latitude.toString(),
+                        location.longitude.toString()
+                    )
                 }
             }
         }
-        val test = fusedLocationClient.lastLocation
-        test.addOnSuccessListener { lsd ->
-            Log.v(TAG, "Test successful")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(TAG, "Activity resumed")
+        enableLocationTracking()
+    }
+
+    private fun enableLocationTracking() {
+        // developer.android.com/training/location/request-updates#request-background-location
+        val permissionAccessFineLocationApproved = ActivityCompat
+            .checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+
+        if (!permissionAccessFineLocationApproved) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                // Show an explanation to the user why we need this permission.
+                // After the user sees the explanation, try to request the permission again.
+                showLocationPermissionRationaleDialog()
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    MY_PERMISSIONS_REQUEST_LOCATION_ID
+                )
+            }
+        } else {
+            // Permission has already been granted
+            Log.d(TAG, "Location permission already granted")
+            requestLocationUpdates()
+        }
+    }
+
+    private fun showLocationPermissionRationaleDialog() {
+        val alert = AlertDialog.Builder(this)
+
+        alert.setTitle("Required Location Permission")
+        alert.setMessage("We need to access your location so we can send you recommendations on the go.")
+        alert.setPositiveButton("OK") { dialog, which ->
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                MY_PERMISSIONS_REQUEST_LOCATION_ID
+            )
+        }
+        alert.setNegativeButton("Cancel") { dialog, which ->
+            dialog.dismiss()
+        }
+        alert.create().show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION_ID -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the task you need to do.
+                    Log.d(TAG, "Location permission granted")
+                    requestLocationUpdates()
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Log.d(TAG, "Location permission denied by user")
+
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+    private fun requestLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+        Log.v(TAG, "Location updates started")
+    }
+
+    private fun createLocationRequest(): LocationRequest {
+        // https://developer.android.com/training/location/change-location-settings
+        Log.v(TAG, "Setting up location services")
+        val locationRequest = LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        // Get current location settings and device capability
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locSettingsResp ->
+            requestingLocationUpdates = locSettingsResp.locationSettingsStates.isGpsPresent &&
+                    locSettingsResp.locationSettingsStates.isGpsUsable &&
+                    locSettingsResp.locationSettingsStates.isLocationPresent &&
+                    locSettingsResp.locationSettingsStates.isLocationUsable
+            Log.v(
+                TAG,
+                "Location settings checked. Able to retrieve location: $requestingLocationUpdates"
+            )
         }
 
-        test.addOnFailureListener() { except ->
-            Log.v(TAG, "Test failed: $except")
+        task.addOnFailureListener { exception ->
+            Log.v(TAG, "Not able to retrieve location due to device settings or capability.")
+            if (exception is ResolvableApiException) {
+                try {
+                    // Show dialog for user to allow location settings
+                    exception.startResolutionForResult(this@ScrollingActivity, 1)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore error
+                }
+            }
         }
+        return locationRequest
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -70,64 +206,5 @@ class ScrollingActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    private fun createLocationRequest(): LocationRequest {
-        // https://developer.android.com/training/location/change-location-settings
-        Log.v(TAG, "Setting up location services")
-        val locationRequest = LocationRequest.create().apply{
-            interval = 10000
-            fastestInterval = 5000
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
-        // Get current location settings
-        val builder = LocationSettingsRequest.Builder()
-            .addLocationRequest(locationRequest)
-        val client: SettingsClient = LocationServices.getSettingsClient(this)
-        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
-
-        task.addOnSuccessListener { locSettingsResp ->
-            requestingLocationUpdates = locSettingsResp.locationSettingsStates.isGpsPresent &&
-                    locSettingsResp.locationSettingsStates.isGpsUsable &&
-                    locSettingsResp.locationSettingsStates.isLocationPresent &&
-                    locSettingsResp.locationSettingsStates.isLocationUsable
-            Log.v(TAG, "Location settings checked. Status: $requestingLocationUpdates") // TODO
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location : Location? ->
-                    Log.v(TAG, "Location: $location")
-                }
-        }
-
-        task.addOnFailureListener { exception ->
-            Log.v(TAG, "Permissions failed")
-            if (exception is ResolvableApiException){
-                try{
-                    // Show dialog for user to allow location settings
-                    exception.startResolutionForResult(this@ScrollingActivity, 1)
-                } catch (sendEx: IntentSender.SendIntentException){
-                    // Ignore error
-                }
-            }
-
-        }
-        return locationRequest
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.v(TAG, "Activity resumed")
-        if ( requestingLocationUpdates ) {
-            Log.v(TAG, "Starting location updates")
-            startLocationUpdates()
-        }
-    }
-
-    private fun startLocationUpdates() {
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            Looper.getMainLooper()
-        )
-        Log.v(TAG, "Location updates started")
     }
 }
